02508af3daacb5f7a64ba606503cd3da081a76b9
Aishwarya-S-23 <aish2327aden@gmail.com>
2026-05-26 21:51:04 +0530

    Add changes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://calculator-deployed-besq.vercel.app/"],
+    allow_origins=["https://calculator-deployed-besq.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
